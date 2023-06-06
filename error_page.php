4b7bf20ecbf3b05123c16174d3989272774b7bf4
ben <26384652+910475syk@users.noreply.github.com>
2023-06-06 17:05:36 +0800

    Create error_page.php

---
+++ error_page.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>ERROR</title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/style.css">
+        <style>
+            h1{
+                margin-top: 200px;
+                font-size: 100px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Oops!!</h1>
+    </body>
+</html>
